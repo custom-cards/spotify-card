046801ca6fa3cce2357916ccd51dc219203c83c4
Niklas Fondberg <niklas.fondberg@gmail.com>
2019-08-03 16:18:58 +0200

    Add HACS support

--- rollup.config.js
+++ rollup.config.js
@@ -25,21 +25,4 @@ export default [
       terser(),
     ],
   },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
-  {
-    input: 'src/spotify-card.js',
-    external: ['ms'],
-    output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
-    plugins: [
-      babel({
-        exclude: ['node_modules/**'],
-      }),
-    ],
-  },
 ];
